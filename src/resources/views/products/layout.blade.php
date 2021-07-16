8c6673369379e1e60d8b462229b8887cf5894255
dunek <losingfireflies23@gmail.com>
2021-07-16 13:18:53 +0200

    added product page view

--- src/resources/views/products/layout.blade.php
+++ src/resources/views/products/layout.blade.php
@@ -5,7 +5,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css">
 </head>
 <body>
-    <h1>Laravel 8 CRUD Application</h1>
     <div class="container">
         <br>
         @yield('content')
